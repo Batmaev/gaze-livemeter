03cf69fdeadd1f5fcca2716e3f8c03bd18914296
Batmaev <vbbatmaev@gmail.com>
2025-12-04 16:35:39 +0300

    normalize generated trajectories

--- backend/stimulus/generate.py
+++ backend/stimulus/generate.py
@@ -1,7 +1,23 @@
+from functools import wraps
+
 import numpy as np
 import scipy.interpolate as si
 
 
+def normalized(func):
+    """Нормализация траекторий в [0, 1]×[0, 1]"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        x, y = func(*args, **kwargs)
+        x -= x.min()
+        y -= y.min()
+        x /= x.max()
+        y /= y.max()
+        return x, y
+    return wrapper
+
+
+@normalized
 def fourier_ios_like(steps=600, harmonics_count=4) -> tuple[np.ndarray, np.ndarray]:
     """
     Воспроизводит логику старых iOS стимулов.
@@ -9,14 +25,14 @@ def fourier_ios_like(steps=600, harmonics_count=4) -> tuple[np.ndarray, np.ndarr
     """
     first_amplitude = np.random.uniform(0.3, 0.8)
     first_phase = np.random.uniform(0.0, 2 * np.pi)
-    coefficients = [(float(first_amplitude), 1.0, float(first_phase))]
+    coefficients = [(first_amplitude, 1.0, first_phase)]
 
     for i in range(2, harmonics_count + 1):
         amplitude_decay = np.random.uniform(4.0, 7.0)
         base_amplitude = first_amplitude * amplitude_decay
         amplitude = base_amplitude / (i * i)
         phase = np.random.uniform(0.0, 2 * np.pi)
-        coefficients.append((float(amplitude), float(i), float(phase)))
+        coefficients.append((amplitude, i, phase))
 
     t = np.linspace(0.0, 2.0 * np.pi, steps)
     x = np.zeros_like(t)
@@ -29,6 +45,7 @@ def fourier_ios_like(steps=600, harmonics_count=4) -> tuple[np.ndarray, np.ndarr
     return x, y
 
 
+@normalized
 def random_spline(steps=600, n_ctrl=8) -> tuple[np.ndarray, np.ndarray]:
     """
     Случайные сплайны, в предварительных экспериментах — самый разнообразный тип траекторий.
